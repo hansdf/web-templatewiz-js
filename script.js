e175dcf12d24965dc88d966b108c238286d5d04e
hansdf <dornsback@gmail.com>
2025-05-09 20:02:48 -0300

    1st commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let templates = [
+    {
+      "Nome": "Introducao",
+      "Conteudo": "Me chamo [Usuario] e estou como responsável pelo seu problema.",
+      "Selecionado": false
+    },
+    {
+      "Nome": "Pedir detalhes",
+      "Conteudo": "Por favor, poderia fornecer mais detalhes sobre o seu problema?",
+      "Selecionado": false
+    },
+    {
+      "Nome": "Pedir passo a passo",
+      "Conteudo": "Poderia descrever o passo a passo para reproduzir o problema?",
+      "Selecionado": false
+    },
+    {
+      "Nome": "Solucao para problema X",
+      "Conteudo": "Para resolver o problema X, tente as seguintes etapas: [Instrução].",
+      "Selecionado": false
+    },
+    {
+      "Nome": "Solucao para problema Y",
+      "Conteudo": "A solução para o problema Y é a seguinte: [Instrução].",
+      "Selecionado": false
+    },
+    {
+      "Nome": "Solucao para problema Z",
+      "Conteudo": "Para o problema Z, siga os seguintes passos: [Instrução].",
+      "Selecionado": false
+    },
+    {
+      "Nome": "Assinatura",
+      "Conteudo": "Atenciosamente, [Seu Nome].",
+      "Selecionado": false
+    }
+  ];
+  
